d0dca1ad5204aedf058f155edd57c14ef6709b57
Michael Fogleman <fogleman@gmail.com>
2016-01-13 15:33:43 -0500

    faster

--- pixsort/model.go
+++ pixsort/model.go
@@ -24,45 +24,86 @@ type Undo struct {
 
 type Model struct {
 	Points []Point
+	Seen   []bool
+	Score  int
 }
 
 func NewModel(points []Point) *Model {
 	model := Model{}
 	model.Points = points
+	model.Seen = make([]bool, len(points))
+	model.Score = 0
+	for i := 0; i < len(points)-1; i++ {
+		model.Score += points[i].DistanceTo(points[i+1])
+	}
 	return &model
 }
 
 func (m *Model) Energy() float64 {
-	total := 0.0
-	for i := 0; i < len(m.Points)-1; i++ {
-		total += float64(m.Points[i].DistanceTo(m.Points[i+1]))
-	}
-	return total
+	return float64(m.Score)
 }
 
 func (m *Model) DoMove() interface{} {
 	n := len(m.Points)
 	i := rand.Intn(n)
 	j := rand.Intn(n)
-	points := m.Points
-	p := points[i]
-	points = append(points[:i], points[i+1:]...)
-	points = append(points[:j], append([]Point{p}, points[j:]...)...)
+	m.Update(i, j, -1)
+	m.Move(i, j)
+	m.Update(i, j, 1)
 	return Undo{i, j}
 }
 
 func (m *Model) UndoMove(undo interface{}) {
 	u := undo.(Undo)
-	j := u.I
-	i := u.J
-	points := m.Points
-	p := points[i]
-	points = append(points[:i], points[i+1:]...)
-	points = append(points[:j], append([]Point{p}, points[j:]...)...)
+	m.Update(u.J, u.I, -1)
+	m.Move(u.J, u.I)
+	m.Update(u.J, u.I, 1)
 }
 
 func (m *Model) Copy() Annealable {
 	points := make([]Point, len(m.Points))
 	copy(points, m.Points)
-	return &Model{points}
+	return &Model{points, m.Seen, m.Score}
+}
+
+func (m *Model) Move(i, j int) {
+	p := m.Points
+	v := p[i]
+	for i < j {
+		p[i] = p[i+1]
+		i++
+	}
+	for i > j {
+		p[i] = p[i-1]
+		i--
+	}
+	p[j] = v
+}
+
+func (m *Model) Update(i, j, sign int) {
+	var indexes []int
+	if i == j {
+		return
+	} else if sign < 0 && i < j {
+		indexes = []int{i - 1, i, j}
+	} else if sign > 0 && i < j {
+		indexes = []int{i - 1, j - 1, j}
+	} else if sign < 0 && i > j {
+		indexes = []int{i - 1, i, j - 1}
+	} else if sign > 0 && i > j {
+		indexes = []int{i, j - 1, j}
+	}
+	for _, a := range indexes {
+		if a < 0 || a >= len(m.Seen)-1 || m.Seen[a] {
+			continue
+		}
+		m.Seen[a] = true
+		m.Score += sign * m.Points[a].DistanceTo(m.Points[a+1])
+	}
+	for _, a := range indexes {
+		if a < 0 || a >= len(m.Seen)-1 {
+			continue
+		}
+		m.Seen[a] = false
+	}
 }
