b2cc70281180d00cbd9568324021d5c5f9bde135
Michael Fogleman <fogleman@gmail.com>
2016-01-13 16:02:09 -0500

    faster, better

--- pixsort/model.go
+++ pixsort/model.go
@@ -9,29 +9,21 @@ type Point struct {
 func (a Point) DistanceTo(b Point) int {
 	dx := a.X - b.X
 	dy := a.Y - b.Y
-	if dx < 0 {
-		dx = -dx
-	}
-	if dy < 0 {
-		dy = -dy
-	}
-	return dx + dy
+	return dx*dx + dy*dy
 }
 
 type Undo struct {
-	I, J int
+	I, J, Score int
 }
 
 type Model struct {
 	Points []Point
-	Seen   []bool
 	Score  int
 }
 
 func NewModel(points []Point) *Model {
 	model := Model{}
 	model.Points = points
-	model.Seen = make([]bool, len(points))
 	model.Score = 0
 	for i := 0; i < len(points)-1; i++ {
 		model.Score += points[i].DistanceTo(points[i+1])
@@ -47,23 +39,23 @@ func (m *Model) DoMove() interface{} {
 	n := len(m.Points)
 	i := rand.Intn(n)
 	j := rand.Intn(n)
+	s := m.Score
 	m.Update(i, j, -1)
 	m.Move(i, j)
 	m.Update(i, j, 1)
-	return Undo{i, j}
+	return Undo{i, j, s}
 }
 
 func (m *Model) UndoMove(undo interface{}) {
 	u := undo.(Undo)
-	m.Update(u.J, u.I, -1)
 	m.Move(u.J, u.I)
-	m.Update(u.J, u.I, 1)
+	m.Score = u.Score
 }
 
 func (m *Model) Copy() Annealable {
 	points := make([]Point, len(m.Points))
 	copy(points, m.Points)
-	return &Model{points, m.Seen, m.Score}
+	return &Model{points, m.Score}
 }
 
 func (m *Model) Move(i, j int) {
@@ -81,29 +73,28 @@ func (m *Model) Move(i, j int) {
 }
 
 func (m *Model) Update(i, j, sign int) {
-	var indexes []int
 	if i == j {
 		return
-	} else if sign < 0 && i < j {
-		indexes = []int{i - 1, i, j}
-	} else if sign > 0 && i < j {
-		indexes = []int{i - 1, j - 1, j}
-	} else if sign < 0 && i > j {
-		indexes = []int{i - 1, i, j - 1}
-	} else if sign > 0 && i > j {
-		indexes = []int{i, j - 1, j}
 	}
+	var indexes []int
+	if sign < 0 {
+		if i < j {
+			indexes = []int{i - 1, i, j}
+		} else {
+			indexes = []int{i - 1, i, j - 1}
+		}
+	} else {
+		if i < j {
+			indexes = []int{i - 1, j - 1, j}
+		} else {
+			indexes = []int{i, j - 1, j}
+		}
+	}
+	n := len(m.Points) - 1
 	for _, a := range indexes {
-		if a < 0 || a >= len(m.Seen)-1 || m.Seen[a] {
+		if a < 0 || a >= n {
 			continue
 		}
-		m.Seen[a] = true
 		m.Score += sign * m.Points[a].DistanceTo(m.Points[a+1])
-	}
-	for _, a := range indexes {
-		if a < 0 || a >= len(m.Seen)-1 {
-			continue
-		}
-		m.Seen[a] = false
 	}
 }
